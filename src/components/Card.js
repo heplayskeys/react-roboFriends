39bdb3a61826416a0f936c25dd8e0cebd494d1fb
heplayskeys <heplayskeys@gmail.com>
2019-03-07 01:26:56 -0800

    Updated card size (Card component).

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ id, username, name, email }) => {
 
 	return (
 		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc" key={id}>
-			<img src={`https://robohash.org/${id}?200x200`} alt={username} />
+			<img src={`https://robohash.org/${id}?size=200x200`} alt={username} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
