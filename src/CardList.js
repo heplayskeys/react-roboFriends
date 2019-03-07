8f9d972ec70bc80073fc915183d4d526f84666d8
heplayskeys <heplayskeys@gmail.com>
2019-03-07 00:08:05 -0800

    Initial push to master. Ready for styling.

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = (props) => {
+
+	return (
+		props.robots.map(robot => {
+			return (
+				<Card {...robot} key={robot.id} />
+			);
+		})
+	);
+}
+
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,65 @@
+const robots = [
+
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz"
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Ehow@melissa.tv"
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "ClemenBauch@yesenia.net"
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Patricia.Lebsack@kory.org"
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "CheesyDie@annie.ca"
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "D.Schulist@jasper.info"
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "KurtyKurtW@billy.biz"
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me"
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Greichert@dana.io"
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Clemons.DuB@karina.biz"
+  }
+];
+
+export default robots;
