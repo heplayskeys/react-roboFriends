eaff573d31a98e2c00b7403e59002b4ec9db72c1
heplayskeys <heplayskeys@gmail.com>
2019-03-30 10:39:32 -0700

    Added momentum scroll to robot list.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
 
 	return (
-		<div style={{overflowY: "scroll", borderTop: "1px solid black", height: "800px"}}>
+		<div style={{overflowY: "scroll", borderTop: "1px solid black", height: "800px", WebkitOverflowScrolling: "touch"}}>
 			{props.children}
 		</div>
 	);
